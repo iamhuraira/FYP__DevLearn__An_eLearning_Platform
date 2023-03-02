23aaf9b1325190ef8b3bec276502aafbb3291077
Abu Huraira <iamhuraira429@gmail.com>
2023-03-02 13:30:55 +0500

    Done initial settings of CSS with scss

---
+++ front-end/src/components/Header/Header.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import logo from '../../assets/img/logo.png'
+import { Navigate, useNavigate } from 'react-router-dom'
+const Header = (props) => {
+    const navigate = useNavigate();
+    return (
+      <div className=''>
+          <div className="logo">
+              <img src={logo} alt="logo" />
+              <div>
+                  <ul>
+                      <li>Courses</li>
+                      <li>Pricing</li>
+                  </ul>
+              </div>
+          </div>
+
+          <button onClick={() => navigate(props.login ? '/login' : '/signup')}>
+              {props.login ? 'Log In' : 'Sign In'}
+          </button>
+      </div>
+  )
+}
+
+export default Header
