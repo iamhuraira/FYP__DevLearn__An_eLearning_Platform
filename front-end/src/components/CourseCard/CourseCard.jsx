9c1071f76a73d8a2109ee06e66838c72a1b0e75e
Abu Huraira <iamhuraira429@gmail.com>
2023-03-19 21:17:26 +0500

    Course Card Done

---
+++ front-end/src/components/CourseCard/CourseCard.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import courseimg from '../../assets/img/CourseImages/python.png'
+import py from '../../assets/img/CourseImages/py.png'
+const CourseCard = () => {
+    return (
+        <div className='fun'>
+            <div className="backgroundCard">
+                <img src={courseimg} alt="" />
+                <div className='courseLogo'>
+                    <img src={py} alt="" srcset="" />
+                </div>
+                <h2>Programming for Everybody (Getting Started with Python)</h2>
+
+                <p>
+                    <span>Beginner </span>
+                    <span>·</span>
+                    <span> 3 Weeks </span>
+                </p>
+
+            </div>
+        
+           
+        </div>
+    )
+}
+
+export default CourseCard
--- front-end/src/pages/LandingPage.jsx
+++ front-end/src/pages/LandingPage.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header from '../components/Header/Header'
 import Footer from '../components/Footer/Footer'
+import CourseCard from '../components/CourseCard/CourseCard'
 
 const LandingPage = () => {
   return (
       <>
       <Header />
+      <CourseCard />
      <Footer />
      
           
