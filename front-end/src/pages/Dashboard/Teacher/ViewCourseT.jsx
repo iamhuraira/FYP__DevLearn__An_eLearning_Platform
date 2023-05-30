994669dc50b9d445080aa654255ea58febc02e9e
Ahmar Farooq <bsef19a528@pucit.edu.pk>
2023-05-30 21:56:42 +0500

    chnages

--- front-end/src/pages/Dashboard/Teacher/ViewCourseT.jsx
+++ front-end/src/pages/Dashboard/Teacher/ViewCourseT.jsx
@@ -1,25 +1,27 @@
-import React, { useState } from 'react'
-import CourseCard from '../../../components/CourseCard/CourseCard'
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import CourseCard from "../../../components/CourseCard/CourseCard";
+import HeaderDashboard from "../../../DashboardComponents/HeaderDashboard";
 
 const ViewCourseT = () => {
-  const [courses, setCourses] = useState([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11])
+  const [courses, setCourses] = useState([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]);
+  const useDatar = useSelector((state) => state.user.userData);
   return (
     <div>
-      <div className='allCourses'>
-        <div className='courseLoop'>
-
-          {
-            courses.map(() => {
-              return(
-              <div className='courseCard'>
+      <HeaderDashboard user={useDatar} />
+      <div className="allCourses">
+        <div className="courseLoop">
+          {courses.map(() => {
+            return (
+              <div className="courseCard">
                 <CourseCard />
-              </div>)
-            })
-          }
+              </div>
+            );
+          })}
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ViewCourseT
+export default ViewCourseT;
